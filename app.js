fdd6460aa9ce1a2a7b42b9608d91848acafb372d
Cairo Stewart <stewartcairo@gmail.com>
2016-01-18 10:28:11 -0800

    setting up all the pages

--- app.js
+++ app.js
@@ -2,16 +2,26 @@ var express 	= require('express'),
 	app			= express(),
 	morgan  	= require('morgan'),
 	locus 		= require('locus'),
-	bodyParser  = require('body-parser');
+	bodyParser  = require('body-parser'),
+	books 		= require('./routes/books'),
+	authors 	= require('./routes/authors');
 
 
-app.use('views engine', 'ejs');
+app.set('view engine', 'ejs');
 app.use(morgan('dev'));
+app.use(express.static('public'))
+
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(function(req, res, next) {
 	if(req.body) {
 		console.log(req.body);
 	}
 	next();
+});
+
+app.get('/', function(req, res) {
+	res.render('pages/index');
 })
-app.use('/books', books)
+app.use('/books', books);
+app.use('/authors', authors);
+app.listen(8000)
